ad470a0f8f66a1eaf2a7ada5a817c217a3f933f0
Kaiqi Chen <68723255+hoikeiblahblahblah@users.noreply.github.com>
2020-08-07 00:03:39 +1000

    Update portrait.js

--- Week 2/portrait.js
+++ Week 2/portrait.js
@@ -1,36 +1,32 @@
 void setup(){
-background(247,198,59);
-size (500, 500);
+size (500,500); //this is the size of the canvas
+background (0); //this chooses our background colour
 }
 
-void draw(){
-if (mousePressed){
-fill (247,198,59);
-noStroke();
-ellipse (mouseX, mouseY, 30, 30);
+void draw(){//portrait
+  ellipse (250,250,160,220);
+  fill (0);
+  ellipse (220,220,30,40);
+  fill(0);
+  ellipse (280,220,30,40);
+  fill(255);
+  ellipse (220,220,10,10);//eyeball
+  fill(255);
+  ellipse (280,220,10,10);//eyeball
+  fill(255,204,204);
+  ellipse (250,300,60,30);//mouth
+  fill(255,255,255);
+  ellipse (170,265,30,40);//ear
+  fill(255,255,255);
+  ellipse (330,265,30,40);//ear
+   
+
+stroke(255); //this choose black as the outline
+line (mouseX, mouseY, 250, 250); 
+fill (255,255,255);
+noStroke(); //turns stroke off
 }
-else {
-fill(0);
-noStroke();
-ellipse (mouseX, mouseY, 30, 30);
-}
 
-fill(230,200,180);
-//the face follows from here
-translate(250,250); 
-//face
-ellipse (0, 0, 150,200); 
-
-//mouth
-fill(mouseX/2, mouseY/2, (mouseX + mouseY)/4);
-ellipse (0, 40, 70,30); 
-noStroke();
-
-//eyes
-fill(255);
-ellipse (30, -40, 40,40);
-ellipse (-30, -40, 40,40);
-fill (0);
-ellipse (0-mouseX/20, -40, 15,15);
-ellipse (mouseX/20, -40, 15,15);
+void keyPressed(){
+background (mouseY/2, (mouseY+mouseX)/4,250-mouseX/2);
 }
